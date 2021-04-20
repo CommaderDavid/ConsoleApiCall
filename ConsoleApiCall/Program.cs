6094c09333ac28210e4b5a4edfa4b25480449a6c
David Boedigheimer <stealth259@gmail.com>
2021-04-20 15:54:20 -0700

    Initial creation of ConsoleApiCall.

---
+++ ConsoleApiCall/Program.cs
@@ -0,0 +1,27 @@
+﻿using RestSharp;
+using System;
+using System.Threading.Tasks;
+
+namespace ApiTest
+{
+    class Program
+    {
+        static void Main()
+        {
+            Task<string> apiCallTask = ApiHelper.ApiCall("[pQQAGOJRk4a7RAz0dLzVPKjWsAQbOh8y]");
+            string result = apiCallTask.Result;
+            Console.WriteLine(result);
+        }
+    }
+
+    class ApiHelper
+    {
+        public static async Task<string> ApiCall(string apiKey)
+        {
+            RestClient client = new RestClient("https://api.nytimes.com/svc/topstories/v2");
+            RestRequest request = new RestRequest($"home.json?api-key={apiKey}", Method.GET);
+            IRestResponse response = await client.ExecuteTaskAsync(request);
+            return response.Content;
+        }
+    }
+}
